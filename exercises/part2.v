a2eb7301fbfad2601774f41f6039c223b01e6ed6
Zohan Subhash <zohan.subhash@gmail.com>
2024-11-04 14:38:20 -0500

    Update part2.v

--- exercises/part2.v
+++ exercises/part2.v
@@ -1,4 +1,10 @@
-module part2(input[17:0] SW, output[17:0] LEDR);
+module part2(SW, LEDR);
+	input [17:0] SW;
+	output [17:0] LEDR;
+	wire [3:0] one;
+	wire [3:0] two;
+	assign one[3:0]=SW[3:0];
+	assign two[3:0]=SW[7:4];
 	assign LEDR[9]=SW[9];
-	assign LEDR[3:0]=SW[9]?SW[7:4]:SW[3:0];
+	assign LEDR[3:0]=(~{4{SW[9]}}&one)|({4{SW[9]}}&two);
 endmodule
