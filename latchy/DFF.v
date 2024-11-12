0c0294b3ad053dff3f366aa03923f8cf4c96722f
Zohan Subhash <zohan.subhash@gmail.com>
2024-11-12 14:17:40 -0500

    Create DFF.v

---
+++ latchy/DFF.v
@@ -0,0 +1,10 @@
+module DFF(input d, input clk, output Q, output NQ);
+	wire CLK1;
+	wire CLK2;
+	wire Q1;
+	wire NQ1;
+	assign CLK2 = clk;
+	assign CLK1 = ~clk;
+	DLat D1(d, CLK1, Q1, NQ1);
+	DLat D2(Q1, CLK2, Q, NQ);
+endmodule
