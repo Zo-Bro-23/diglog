c69dff2afaf75dd40abf0a316f54811fdaa92b4d
Zohan Subhash <zohan.subhash@gmail.com>
2024-11-12 14:28:31 -0500

    Update Latchy.v

--- latchy/Latchy.v
+++ latchy/Latchy.v
@@ -1,5 +1,5 @@
 module Latchy(input [17:0] SW, output [17:0] LEDR);
 	SR mod1(SW[0], SW[1], LEDR[0], LEDR[1]);
 	DLat mod2(SW[2], SW[3], LEDR[2], LEDR[3]);
-	DFF mod3(SW[4], SW[5], LEDR[4], LEDR[5]);
+	DFFl mod3(SW[4], SW[5], LEDR[4], LEDR[5]);
 endmodule
