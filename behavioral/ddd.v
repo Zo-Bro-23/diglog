a02c05715127fe54c4d93e63f58e6a65f4b18947
Zohan Subhash <zohan.subhash@gmail.com>
2024-12-20 15:10:08 -0500

    Update ddd.v

--- behavioral/ddd.v
+++ behavioral/ddd.v
@@ -1,6 +1,10 @@
-module ddd(input clk, input d, output reg qa, output reg qb, output reg qc);
+module ddd(input [3:0] KEY, output [2:0] LEDR);
+	dddmain(~KEY[0], ~KEY[3], LEDR[0], LEDR[1], LEDR[2]);
+endmodule
+
+module dddmain(input clk, input d, output reg qa, output reg qb, output reg qc);
 	always @ (clk) begin
-		if(clk) begin
+		if (clk) begin
 			qa <= d;
 		end
 	end
