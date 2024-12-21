d38de4d8f8d7c5400292195bc8e9ed3bff485f3b
Zohan Subhash <zohan.subhash@gmail.com>
2024-12-21 12:38:01 -0500

    Update slowcounter.v

--- behavioral/slowcounter.v
+++ behavioral/slowcounter.v
@@ -5,11 +5,18 @@ module slowcounter(input [1:0] SW, input CLOCK_50, input [0:0] KEY, output [6:0]
 endmodule
 
 module slowmain(input CLR, input CLK, output [15:0] Q);
-	wire [15:0] Q1;
-	wire [3:0] Q2;
-	countermodule(CLR, 1'b1, CLK, Q1);
-	smallcounter(~|Q1, CLK, CLR, Q2);
-	countermodule(CLR, ~|Q1, CLK, Q);
-	//countermodule(CLR, ~|Q2, CLK, Q);
-	//countermodule(CLR, ~|Q3, CLK, Q);
+	wire [23:0] Q1;
+	customcounter(CLR, 1'b1, CLK, Q1);
+	countermodule(CLR, 1'b1, ~|Q1, Q);
+endmodule
+
+module customcounter(input CLR, input EN, input CLK, output reg [23:0] Q);		
+	always @ (posedge CLK) begin
+		if (CLR) begin
+			Q <= 16'b000000000000000000000000;
+		end
+		else if (EN) begin
+			Q <= Q + 1;
+		end
+	end
 endmodule
