66c6e281aad67f0d9f988968358bc07b66fb5240
Zohan Subhash <zohan.subhash@gmail.com>
2025-02-04 21:02:36 -0500

    Update main.v

--- makingMemories/main.v
+++ makingMemories/main.v
@@ -1,13 +1,11 @@
 module memory(input [2:0] D, input [1:0] SEL, input E, output [2:0] Q);
 	wire [3:0] wSEL;
 	wire [3:0] rSEL;
-	wire [3:0] tQ;
+	wire [2:0] tQ [3:0];
 	decoder(SEL, E, wSEL[0], wSEL[1], wSEL[2], wSEL[3], rSEL[0], rSEL[1], rSEL[2], rSEL[3]);
-	word(D, wSEL[0], tQ[0]);
-	word(D, wSEL[1], tQ[1]);
-	word(D, wSEL[2], tQ[2]);
-	word(D, wSEL[3], tQ[3]);
-	assign Q <= (tQ[0] & rSEL[0]) | (tQ[1] & rSEL[1]) | (tQ[2] & rSEL[2]) | (tQ[3] & rSEL[3]);
+	word WRDINST [3:0] (D, wSEL, tQ);
+	//assign Q = (tQ[0] & {4{rSEL[0]}}) | (tQ[1] & {4{rSEL[1]}}) | (tQ[2] & {4{rSEL[2]}}) | (tQ[3] & {4{rSEL[3]}});
+	assign Q = |(tQ & {3{rSEL}});
 endmodule
 
 module decoder(input [1:0] SEL, input E, output reg A, B, C, D, output reg rA, rB, rC, rD);
@@ -58,12 +56,10 @@ module decoder(input [1:0] SEL, input E, output reg A, B, C, D, output reg rA, r
 endmodule
 
 module word(input [2:0] D, input E, output [2:0] Q);
-	D1 DLatch(D[0], E, Q[0]);
-	D2 DLatch(D[1], E, Q[1]);
-	D3 DLatch(D[2], E, Q[2]);
+	DDLatch DINST [2:0] (D, E, Q);
 endmodule
 
-module DLatch(input D, input E, output reg Q);
+module DDLatch(input D, input E, output reg Q);
 	always @ (D or E) begin
 		if (E) Q <= D;
 		else Q <= Q;
